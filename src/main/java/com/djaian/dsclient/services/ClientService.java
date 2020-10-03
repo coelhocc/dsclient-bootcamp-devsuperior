e6450ea534cdea08390093c804c834f45bbf4734
Jean Coelho <coelhocc@gmail.com>
2020-10-02 22:10:30 -0300

    Client by id, exception handling

--- src/main/java/com/djaian/dsclient/services/ClientService.java
+++ src/main/java/com/djaian/dsclient/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.djaian.dsclient.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.djaian.dsclient.entities.Client;
 import com.djaian.dsclient.entities.dto.ClientDTO;
 import com.djaian.dsclient.repositories.ClientRepository;
+import com.djaian.dsclient.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -20,5 +22,11 @@ public class ClientService {
 		List<Client> list =  repository.findAll();
 		
 		return list.stream().map(cli -> new ClientDTO(cli)).collect(Collectors.toList());
+	}
+
+	public ClientDTO findById(Long id) {
+		Optional<Client> cliObj = repository.findById(id);
+		Client entity = cliObj.orElseThrow(() -> new ResourceNotFoundException("ID not found " + id));
+		return new ClientDTO(entity);
 	}
 }
