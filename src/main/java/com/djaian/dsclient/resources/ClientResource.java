4a710a1f2a35d1c549f743704d4e3adf43328e71
Jean Coelho <coelhocc@gmail.com>
2020-10-02 23:44:56 -0300

    Post Client

--- src/main/java/com/djaian/dsclient/resources/ClientResource.java
+++ src/main/java/com/djaian/dsclient/resources/ClientResource.java
@@ -1,13 +1,18 @@
 package com.djaian.dsclient.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.djaian.dsclient.entities.dto.ClientDTO;
 import com.djaian.dsclient.services.ClientService;
@@ -29,5 +34,12 @@ public class ClientResource {
 	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
 		ClientDTO dto = service.findById(id); 
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+		dto = service.insert(dto); 
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
--- src/main/java/com/djaian/dsclient/services/ClientService.java
+++ src/main/java/com/djaian/dsclient/services/ClientService.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.djaian.dsclient.entities.Client;
 import com.djaian.dsclient.entities.dto.ClientDTO;
@@ -18,15 +19,34 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<ClientDTO> findAll() {
 		List<Client> list =  repository.findAll();
 		
 		return list.stream().map(cli -> new ClientDTO(cli)).collect(Collectors.toList());
 	}
 
+	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> cliObj = repository.findById(id);
 		Client entity = cliObj.orElseThrow(() -> new ResourceNotFoundException("ID not found " + id));
 		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		copyDtoToClient(dto, entity);
+		entity = repository.save(entity);
+		
+		return new ClientDTO(entity);
+	}
+
+	private void copyDtoToClient(ClientDTO dto, Client entity) {
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
 	}
 }
